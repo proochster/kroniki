82bf7fc664098b6643a9c02419ddbcbdb36bedcf
proochster <proochster@gmail.com>
2021-02-15 17:18:18 +0000

    ^ SW Config

--- _includes/js/service-worker.js
+++ _includes/js/service-worker.js
@@ -26,68 +26,64 @@ self.addEventListener('message', (event) => {
  */
 self.__precacheManifest = [
   {
-    "url": "404.html",
-    "revision": "35c20df57cbb0b8d1ab73df7b5831a4e"
-  },
-  {
     "url": "assets/theme/icons/kroniki-icon-114x114.png",
-    "revision": "6ac8732c1919484f4890b56e95d24879"
+    "revision": "41fca34059820df24254b90a773da2ab"
   },
   {
     "url": "assets/theme/icons/kroniki-icon-120x120.png",
-    "revision": "519ead36e68a3b73c7386aaf33f69bff"
+    "revision": "1ff6f344971af525cb4b9f2eef76bbbe"
   },
   {
     "url": "assets/theme/icons/kroniki-icon-144x144.png",
-    "revision": "742e59e7f1cea398dfd56dbd6b504b31"
+    "revision": "cb8ec8f79ce5d50f3bf7f7ac38aa6410"
   },
   {
     "url": "assets/theme/icons/kroniki-icon-152x152.png",
-    "revision": "9bf65b62f1b105cff96d2d288b59b7a6"
+    "revision": "f86568fc4741915edd373d3005797665"
   },
   {
     "url": "assets/theme/icons/kroniki-icon-168x168.png",
-    "revision": "fdfb4faa7f8a24c6497768c75a3cc871"
+    "revision": "911b2c408d37503bc18ef0ae9bc77841"
   },
   {
     "url": "assets/theme/icons/kroniki-icon-16x16.png",
-    "revision": "cb318ebb0936e8e110839591229396d9"
+    "revision": "4df65aa050e3e0dfdf60c9e4285fa8e7"
   },
   {
     "url": "assets/theme/icons/kroniki-icon-180x180.png",
-    "revision": "4d0d6e623229657e944ab9657f112ca9"
+    "revision": "7fef7a5400a1710f4a9b0d4cc76f9ab9"
   },
   {
     "url": "assets/theme/icons/kroniki-icon-192x192.png",
-    "revision": "f1678b0d72933559637c7fcb7a88d64e"
+    "revision": "67059c79fd017e94cae01fe00486bf82"
   },
   {
     "url": "assets/theme/icons/kroniki-icon-32x32.png",
-    "revision": "c11de2a8dd79eeb15b321c4377ed27dc"
+    "revision": "fa896f0e8c31a293b5b15cd4cefe3959"
   },
   {
     "url": "assets/theme/icons/kroniki-icon-48x48.png",
-    "revision": "a216557ae6a958f330788209adbd337a"
+    "revision": "633b4363cfb7093c203f4c616100c3cb"
   },
   {
     "url": "assets/theme/icons/kroniki-icon-512x512.png",
-    "revision": "fe20c5c4da42b100bda9dc08a84086f5"
+    "revision": "115ad072c87671164f8f0182948d1f28"
   },
   {
     "url": "assets/theme/icons/kroniki-icon-57x57.png",
-    "revision": "ef714bb484dac284abc4568fbc1ab924"
+    "revision": "3a15aa3d8fbc4014843477b4f199f393"
   },
   {
     "url": "assets/theme/icons/kroniki-icon-60x60.png",
-    "revision": "b80b2512224417ef33694b3105fddd4e"
+    "revision": "544c0f1e3ddb285c90ffc098b7cab98f"
   },
   {
     "url": "assets/theme/icons/kroniki-icon-72x72.png",
-    "revision": "7c2a0f8a1bbdb34e079fa819cabc4c89"
+    "revision": "6c45c5f5e6e98a9f638642f003f2121d"
   },
   {
     "url": "assets/theme/icons/kroniki-icon-96x96.png",
-    "revision": "d5b0472d8ca44956a6697f6473d1ee44"
+    "revision": "9b1f369a8da0b58c7881b4dedfb2261d"
   },
   {
     "url": "assets/theme/intro_large.jpeg",
@@ -98,16 +94,8 @@ self.__precacheManifest = [
     "revision": "9dc4eecbcdb0d3eb4e9a008e503a9267"
   },
   {
-    "url": "favicon.png",
-    "revision": "593e91841822ebb418a27f5cad4464d4"
-  },
-  {
     "url": "feed.xml",
-    "revision": "788e874e287cb73fee46f7f834dcf971"
-  },
-  {
-    "url": "index.html",
-    "revision": "97f35114b12ed838097f6c475f726f46"
+    "revision": "cf0f140534b0fb1eaa8c54b5dfb8cde6"
   },
   {
     "url": "main.js",
@@ -115,27 +103,11 @@ self.__precacheManifest = [
   },
   {
     "url": "manifest.json",
-    "revision": "68c8c442811b6bcc8275a4a3f876e8d9"
+    "revision": "7e19381227ae707c15f4c8c54d42c812"
   },
   {
     "url": "Readme.md",
-    "revision": "0cc773fff51d1e59638ae296a4321d49"
-  },
-  {
-    "url": "stories/en/test_3.html",
-    "revision": "294813934a23c352bbb3d79cec00bc45"
-  },
-  {
-    "url": "stories/en/test.html",
-    "revision": "a006d8f0145800d70f0f4929dc5ba668"
-  },
-  {
-    "url": "stories/en/test2.html",
-    "revision": "d19402d50840d14a817b41ff1d3a8b64"
-  },
-  {
-    "url": "stories/pl/gawron.html",
-    "revision": "c07ca1ed34dfdbb821cc602955f1f76f"
+    "revision": "d6bfd272519e998587713818a4a1f763"
   },
   {
     "url": "workbox-config.js",
